08fed476495bfee85ea8c252b0f4e49e1589be54
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-11 11:18:05 +0200

    array mis en commentaire

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 echo "saluut !";
 $rep = array();
-foreach($rep as $r){
+/* foreach($rep as $r){
   if (isset($r)){
     print_r($r);
   }
   else {
     echo "pas de fichiers déso";
   }
-}
+} */
 ?>
