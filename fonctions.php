51e1c037b14009b646893c0d3a9c59670fbdbddd
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-12 11:12:32 +0200

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -1,6 +1,27 @@
 <?php
 
 function pagenavbar($page){
-  echo $page;
+  $rep = <<<EOD
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            
+
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="page01.php">Page d'accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="page02.php">Formulaire</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="page04.php">Informations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="page05.php">Placeholder</a>
+                </li>
+            </ul>
+            </nav>
+    EOD; 
+  $rep = str_replace("Placeholder", $page, $rep);
+  return $rep;
 }
 ?>
