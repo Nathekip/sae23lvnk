b9dcb2e5f7bb04cddf8e14f3db0c488b7187d5e7
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-14 11:57:15 +0200

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -12,15 +12,15 @@ function pagenavbar($page){
 
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item p01">
-                    <a class="nav-link" href="index.php">Page d'accueil</a>
+                    <a class="nav-link p01" href="index.php">Page d'accueil</a>
                 </li>
                 <li class="nav-item p02">
                     <a class="nav-link" href="page02.php">Formulaire</a>
                 </li>
                 <li class="nav-item p03">
-                    <a class="nav-link" href="page04.php">Informations</a>
+                    <a class="nav-link active" href="page04.php">Informations</a>
                 </li>
-                <li class="nav-item p04">
+                <li class="nav-item p04 active">
                     <a class="nav-link" href="page05.php">Panier</a>
                 </li>
             </ul>
