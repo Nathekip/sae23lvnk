9ba9a5f03405bf078ae7bfab30baaea444ece6ac
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-19 15:22:54 +0200

    Update test.php

--- test.php
+++ test.php
@@ -9,8 +9,6 @@
     echo '<header>
         <div class="container-fluid bg-info text-center py-3 d-flex justify-content-between align-items-center">
         <div class="d-flex align-items-center mx-auto">
-        <a href="page01.php"><img class="rounded mr-3" src="images/logo.png" alt="logo"></a>
-        <lass="mb-0 flex-fill text-center">Biblionet</h1>
         </div>';
         if(isset($_SESSION['utilisateur'])){
             echo $_SESSION['utilisateur'];
