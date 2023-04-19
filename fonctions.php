9ff7f9df6e937e91ddec9affadea0e3938584c25
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-19 09:55:01 +0200

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -79,7 +79,26 @@ function pageheader(){
 
               <!-- Modal body -->
               <div class="modal-body text-center">
-                connecform
+
+
+                         <div class="container-fluid bg-info text-center py-3 d-flex justify-content-between align-items-center">
+                            <div class="d-flex align-items-center mx-auto">
+                            <div class="login-form">
+                              <form action="connexion_traitement.php" method="post">
+                                <div class="form-group">
+                                  <label>Utilisateur</label>
+                                  <input type="text" class="form-control" name="utilisateur" placeholder="Utilisateur">
+                                </div>
+                                <div class="form-group">
+                                  <label>Mot de passe</label>
+                                  <input type="password" class="form-control" name="motdepasse" placeholder="Mot de passe">
+                                </div>
+                                <button type="submit" class="btn">Se connecter</button>
+                              </form>
+                            </div>
+                          </div>
+
+
               </div>
 
               <!-- Modal footer -->
