169470518b91ebec9b260a650a2ba9527e422888
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-19 10:45:29 +0200

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -117,4 +117,21 @@ function pageheader(){
   }
     
 
+  function addUser($usr, $mdp, $role="user"){
+    $user = array();
+    $json = file_get_contents('data/users.json');
+    $user = json_decode($json, true);
+
+    $add = array();
+    $add['user']=$usr;
+    $add['mdp']=password_hash($mdp,PASSWORD_DEFAULT);
+    $add['role']=$role;
+    $user[$add['user']]=$add;
+
+    $jsonString = json_encode($user, JSON_PRETTY_PRINT);
+    $fp = fopen("data/users.json", 'w');
+    fwrite($fp, $jsonString);
+    fclose($fp);
+}
+
 ?>
