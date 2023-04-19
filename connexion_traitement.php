d428d72637fd9f8b4abfa2a6e546281270997b47
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-19 13:23:40 +0200

    Update connexion_traitement.php

--- connexion_traitement.php
+++ connexion_traitement.php
@@ -14,16 +14,19 @@
     $json = file_get_contents('data/users.json');
     $user = json_decode($json, true);
     $connec = true;
+    $page = "Location: ".$_POST['page'].".php";
+    echo $page;
 
     foreach($user as $u){
     if ((password_verify($_POST['motdepasse'],$u['mdp'])==1) && ($_POST['utilisateur']==$u['user']))
     {
         $_SESSION['utilisateur']=$_POST['utilisateur'];
         $_SESSION['role']=$u['role'];
-        header('Location: page01.php');
+        header($page);
     }
     }
     echo "a";
+    header('Location: page01.php');
 ?>
 </body>
 </html>
