7dccf49189af1f29dc36c01f3d695af2eae58233
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-19 15:41:48 +0200

    Update test.php

--- test.php
+++ test.php
@@ -12,23 +12,48 @@
       echo $btndeco;
         }
      else { 
-       echo '<button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#myModal">
-                Connexion
-              </button>
+       $boutons = 'Vous n\'êtes pas connectés
+          <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#myModal">
+            Connexion
+          </button>
+        </div>
+        <!-- The Modal -->
+        <div class="modal fade" id="myModal">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <!-- Modal Header -->
+              <div class="modal-header">
+                <h4 class="modal-title">Connexion (jaimerais que ce texte soit centré svp)</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+              </div>
+              <!-- Modal body -->
+              <div class="modal-body text-center">
+                         <div class="container-fluid text-center py-3 d-flex justify-content-between align-items-center">
+                            <div class="d-flex align-items-center mx-auto">
+                            <div class="login-form">
+                              <form action="NUMERODEPAGE.php" method="post">
+                                <div class="form-group">
+                                  <label>Utilisateur</label>
+                                  <input type="text" class="form-control" name="utilisateur" placeholder="Utilisateur">
+                                </div>
+                                <div class="form-group">
+                                  <label>Mot de passe</label>
+                                  <input type="password" class="form-control" name="motdepasse" placeholder="Mot de passe">
+                                </div>
+                                <button type="submit" name="page" value=NUMERODEPAGE class="btn btn-success">Se connecter</button>
+                              </form>
+                              <div><a href="creerprofil.php">Pas de profil ? (décaler à gauche)</a></div>
+                              <div><a href="creerprofil.php">Mot de passe oublié ? (décaler à droite)</a></div>
+                            </div>
+                          </div>
+                          
+              </div>
+              <!-- Modal footer -->
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fermer</button>
+              </div>
             </div>
-            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="StudentModalLabel" aria-hidden="true" data-backdrop="static">
-               <div class="modal-dialog">
-                  <div class="modal-content">
-                     <form action="~/GetStudent" class="form-horizontal" role="form" method="post" id="frmStudent">
-                        <div class="modal-footer">
-                           <div class="pull-right">
-                              <button type="submit" class="btn btn-success"><i class="glyphicon glyphicon-ok"></i> Save</button>
-                              <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="glyphicon glyphicon-remove"></i> Close</button>
-                           </div>
-                        </div>
-                     </form>
-                  </div>
-               </div>';   
+           </div>';  
        
         $boutons = str_replace('NUMERODEPAGE', basename($_SERVER["SCRIPT_NAME"], ".php"), $boutons);
         echo $boutons;
