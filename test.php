8d8504775e7613efa15b1a6c38529be9f0e907da
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-19 15:36:49 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,65 +4,16 @@
     <?php
     include('fonctions.php');
     setup();
-
-
-    echo '<header>
-        <div class="container-fluid bg-info text-center py-3 d-flex justify-content-between align-items-center">
-        <div class="d-flex align-items-center mx-auto">
-        </div>';
-        if(isset($_SESSION['utilisateur'])){
-            echo $_SESSION['utilisateur'];
-            $btndeco = '<form action="deconnexion.php" method="post">
-            <button type="submit" name="page" value=NUMERODEPAGE class="btn btn-warning btn-sm">Se déconnecter</button>
-            </form>';
-            $btndeco = str_replace('NUMERODEPAGE', basename($_SERVER["SCRIPT_NAME"], ".php"), $btndeco);
-            echo $btndeco;
+    if(isset($_SESSION['utilisateur'])){
+      echo $_SESSION['utilisateur'];
+      $btndeco = '<form action="deconnexion.php" method="post">
+      <button type="submit" name="page" value=NUMERODEPAGE class="btn btn-warning btn-sm">Se déconnecter</button>
+      </form>';
+      $btndeco = str_replace('NUMERODEPAGE', basename($_SERVER["SCRIPT_NAME"], ".php"), $btndeco);
+      echo $btndeco;
         }
-        else { 
-            $boutons_old = 'Vous n\'êtes pas connectés
-              <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#myModal">
-                Connexion
-              </button>
-            </div>
-            <!-- The Modal -->
-            <div class="modal fade" id="myModal">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <!-- Modal Header -->
-                  <div class="modal-header">
-                    <h4 class="modal-title">Connexion (jaimerais que ce texte soit centré svp)</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                  </div>
-                  <!-- Modal body -->
-                  <div class="modal-body text-center">
-                             <div class="container-fluid text-center py-3 d-flex justify-content-between align-items-center">
-                                <div class="d-flex align-items-center mx-auto">
-                                <div class="login-form">
-                                  <form action="NUMERODEPAGE.php" method="post">
-                                    <div class="form-group">
-                                      <label>Utilisateur</label>
-                                      <input type="text" class="form-control" name="utilisateur" placeholder="Utilisateur">
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Mot de passe</label>
-                                      <input type="password" class="form-control" name="motdepasse" placeholder="Mot de passe">
-                                    </div>
-                                    <button type="submit" name="page" value=NUMERODEPAGE class="btn btn-success">Se connecter</button>
-                                  </form>
-                                  <div><a href="creerprofil.php">Pas de profil ? (décaler à gauche)</a></div>
-                                  <div><a href="creerprofil.php">Mot de passe oublié ? (décaler à droite)</a></div>
-                                </div>
-                              </div>
-
-                  </div>
-                  <!-- Modal footer -->
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fermer</button>
-                  </div>
-                </div>
-               </div>';
-          
-          $boutons = 'Vous n\'êtes pas connectés
+     else { 
+       echo 'Vous n\'êtes pas connectés
               <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#myModal">
                 Connexion
               </button>
@@ -79,7 +30,7 @@
                         </div>
                      </form>
                   </div>
-               </div>            
+               </div>';   
           
           
           
