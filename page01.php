e833baaa4595941f23c1125554d0b9f5fdfada8a
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-19 14:16:06 +0200

    Update page01.php

--- page01.php
+++ page01.php
@@ -5,40 +5,7 @@
     include('fonctions.php');
     setup();
     pageheader();
-    echo pagenavbar("p01");
-    /* echo <<<EOD
-
-      <div class="container mt-3">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-          Open modal
-        </button>
-      </div>
-
-      <!-- The Modal -->
-      <div class="modal" id="myModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
-
-            <!-- Modal Header -->
-            <div class="modal-header">
-              <h4 class="modal-title">Modal Heading</h4>
-              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-
-            <!-- Modal body -->
-            <div class="modal-body">
-              Répète sans chouiner ?
-            </div>
-
-            <!-- Modal footer -->
-            <div class="modal-footer">
-              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-            </div>
-
-          </div>
-        </div>
-      </div>
-      EOD; */
+    pagenavbar("p01");
     ?>
   </body>
 </html>
