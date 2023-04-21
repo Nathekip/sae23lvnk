bd99570f4997532186e4ba17e28b9079dd7f17ad
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-21 09:15:17 +0200

    Update test2.php

--- test2.php
+++ test2.php
@@ -22,6 +22,26 @@ function myFunction() {
   document.getElementById("demo").innerHTML = text;
 }
 </script>
+  
+  
+                          <div class="container-fluid text-center py-3 d-flex justify-content-between align-items-center bg-white">
+                            <div class="d-flex align-items-center mx-auto">
+                            <div class="login-form">
+                              <form action="NUMERODEPAGE.php" id="login-form" method="post">
+                                <div class="form-group">
+                                  <label>Utilisateur</label>
+                                  <input type="text" class="form-control" name="utilisateur" placeholder="Utilisateur">
+                                </div>
+                                <div class="form-group">
+                                  <label>Mot de passe</label>
+                                  <input type="password" class="form-control" name="motdepasse" placeholder="Mot de passe">
+                                </div>
+                                <button type="submit" name="page" value=NUMERODEPAGE class="btn btn-success data-bs-toggle='modal' data-bs-target='#myModal'">Se connecter</button>
+                              </form>
+                              <div><a href="creerprofil.php">Pas de profil ? (décaler à gauche)</a></div>
+                              <div><a href="creerprofil.php">Mot de passe oublié ? (décaler à droite)</a></div>
+                            </div>
+                          </div>
 
 </body>
 </html> 
