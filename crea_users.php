623d505fea027971fde887849cd25b6424bfdb46
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-21 18:19:17 +0100

    Update crea_users.php

--- crea_users.php
+++ crea_users.php
@@ -9,12 +9,12 @@ function newUsers(){
     fwrite($fp, "");
     fclose($fp);
 
-    addUser('admin', 'bonjour', 'admin');
-    addUser('Georges', 'bonjour');
-    addUser('Kono', 'bonjour');
-    addUser('hjeunecrack', 'bonjour');
-    addUser('foret', 'bonjour', 'visitor');
-    addUser('Emilie', 'bonjour', 'admin');
+    addUser('admin', 'bonjour', 'admin@gmail.com', 'admin');
+    addUser('Georges', 'bonjour', 'georges47@hotmail.com');
+    addUser('Kono', 'bonjour', 'konolafripouilledu22@gmail.com');
+    addUser('hjeunecrack', 'bonjour', 'hjeune@crack.org');
+    addUser('foret', 'bonjour', 'arbre@ecosia.com', 'visitor');
+    addUser('Emilie', 'bonjour', 'emilieflocon@laposte.net', 'admin');
 
 }
 newUsers();
