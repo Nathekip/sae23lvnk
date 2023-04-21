0ab53c604839e7ff5d14d1cfa6fdd3f980be8664
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-21 13:37:13 +0200

    Update creerprofil.php

--- creerprofil.php
+++ creerprofil.php
@@ -63,6 +63,8 @@
 
                       <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                         <button type="button" class="btn btn-primary btn-lg">S'inscrire</button>
+                        <button type="button" class="btn btn-info btn-lg">S'inscrire</button>
+                        <button type="button" class="btn btn-dark text-white btn-lg">S'inscrire</button>
                       </div>
 
                     </form>
