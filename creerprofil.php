cbb03b6ae17670577fb210fdb1def6fa08e86978
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-21 17:01:27 +0100

    Update creerprofil.php

--- creerprofil.php
+++ creerprofil.php
@@ -20,7 +20,9 @@
                         <?php
                             echo "<div class='container'>";
                             # alerte Champ vide
-                            if ( in_array("",$_POST) && ( !isset($_POST['condu']) ) ){
+                            $champs = $_POST;
+                            print_r($champs);
+                            if ( in_array("",$_POST) ){
                                 echo "<div class='alert alert-danger'>
                                         <strong>Erreur</strong> Vous n'avez pas rempli tous les champs.
                                       </div>";
