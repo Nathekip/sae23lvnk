45827ed20bc5b13342197ba9492f3847c65f6cac
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-21 13:43:21 +0200

    Update creerprofil.php

--- creerprofil.php
+++ creerprofil.php
@@ -9,6 +9,7 @@
 </head>
 <body>
      
+    <br><br><br>
     <section class="vh-100 bg-light">
       <div class="container h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
@@ -17,11 +18,11 @@
               <div class="card-body p-md-5">
                 <div class="row justify-content-center">
                   <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
+                      <img src="images/Register.jpg" class="img-fluid rounded" alt="Sample image">
+                  </div>
+                  <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
                     <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Créer un compte</p>
-
                     <form class="mx-1 mx-md-4">
-
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
@@ -29,7 +30,6 @@
                           <input type="text" id="form3Example1c" class="form-control" />
                         </div>
                       </div>
-
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
@@ -37,7 +37,6 @@
                           <input type="email" id="form3Example3c" class="form-control" />
                         </div>
                       </div>
-
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
@@ -45,7 +44,6 @@
                           <input type="password" id="form3Example4c" class="form-control" />
                         </div>
                       </div>
-
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
@@ -53,26 +51,16 @@
                           <input type="password" id="form3Example4cd" class="form-control" />
                         </div>
                       </div>
-
                       <div class="form-check d-flex justify-content-center mb-5">
                         <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
                         <label class="form-check-label" for="form2Example3">
                           J'accepte les <a href="#!">Conditions d'utilisation</a>
                         </label>
                       </div>
-
                       <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                         <button type="button" class="btn btn-dark text-white btn-lg">S'inscrire</button>
                       </div>
-
                     </form>
-
-                  </div>
-                  <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                    <img src="images/Register.jpg"
-                      class="img-fluid rounded" alt="Sample image">
-
                   </div>
                 </div>
               </div>
