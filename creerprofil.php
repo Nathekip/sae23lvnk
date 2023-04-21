5c48c5af8deeb19b81897522c052b0ba01fdb420
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-21 13:40:41 +0200

    Update creerprofil.php

--- creerprofil.php
+++ creerprofil.php
@@ -71,7 +71,7 @@
                   <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
                     <img src="images/Register.jpg"
-                      class="img-fluid" alt="Sample image">
+                      class="img-fluid rounded" alt="Sample image">
 
                   </div>
                 </div>
