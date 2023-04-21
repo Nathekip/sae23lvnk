9861c876df01d0987685d0c75061a35626c2fe64
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-21 16:39:59 +0200

    Update creerprofil.php

--- creerprofil.php
+++ creerprofil.php
@@ -18,12 +18,12 @@
                     <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
                         Créer un compte
                         <?php
-                        /* if (not isset($_POST['condu']){
-                            echo "<div class='container'>
-                                      <div class='alert alert-danger'>
-                                          <strong>Erreur</strong> Veuillez accepter les Conditions d'utilisation.
-                                      </div>";
-                        }*/ 
+                            if (not isset($_POST['condu']){
+                                /*echo "<div class='container'>
+                                          <div class='alert alert-danger'>
+                                              <strong>Erreur</strong> Veuillez accepter les Conditions d'utilisation.
+                                          </div>";
+                            */} 
                         ?>
                     </p>
               <div>
