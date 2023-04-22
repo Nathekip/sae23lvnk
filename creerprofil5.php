245f2257c0623d66b6db2214c2970027ecdba6d7
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-22 09:34:45 +0100

    Update creerprofil5.php

--- creerprofil5.php
+++ creerprofil5.php
@@ -18,12 +18,12 @@
                     <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
                         Créer un compte
                         <?php
-                        $formulaire = <<<EOD
+                        $formulaire = '
                                             <div>test</div>
-                                            EOD;
+                                            ';
                         
                         
-                        $formulaire = <<<EOD
+                        /*$formulaire = <<<EOD
                                               <div>
                                                 <div class="row justify-content-center">
                                                   <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
