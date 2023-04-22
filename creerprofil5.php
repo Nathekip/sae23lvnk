bffb60c0fec956da8f57c2d43320b9b85857aaa9
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-22 06:43:55 +0100

    Update creerprofil5.php

--- creerprofil5.php
+++ creerprofil5.php
@@ -80,7 +80,7 @@
                       <img src="images/Register.jpg" class="img-fluid rounded" alt="Sample image">
                   </div>
                   <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                    <form action="creerprofil.php" method="post" class="mx-1 mx-md-4">
+                    <form action="creerprofil5.php" method="post" class="mx-1 mx-md-4">
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
