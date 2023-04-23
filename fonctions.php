0c75b9a1d8811cc9b5969482f0727c16aaa5172d
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-23 10:47:06 +0100

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -87,32 +87,31 @@ function pageheader(){
 
               <!-- Modal body -->
               <div class="modal-body text-center">
-
-
-                         <div class="container-fluid text-center py-3 d-flex justify-content-between align-items-center bg-white">
-                            <div class="d-flex align-items-center mx-auto">
-                            <div class="login-form">
-                              <form action="NUMERODEPAGE.php" id="login-form" method="post">
-                                <div class="form-group">
-                                  <label>Utilisateur</label>
-                                  <input type="text" class="form-control" name="utilisateur" placeholder="Utilisateur">
-                                </div>
-                                <div class="form-group">
-                                  <label>Mot de passe</label>
-                                  <input type="password" class="form-control" name="motdepasse" placeholder="Mot de passe">
-                                </div>
-                                <button type="submit" name="page" value=NUMERODEPAGE class="btn btn-success data-bs-toggle="modal" data-bs-target="#myModal"">Se connecter</button>
-                              </form>
-                              <div class="d-flex justify-content-between w-100 mt-3">
-                                <a href="creerprofil5.php">Pas de profil ?</a>
-                                <a href="creerprofil.php">Mot de passe oublié ?</a></div>
-                            </div>
-                          </div>
-                          
-
-
+                <div class="container-fluid text-center py-3 d-flex justify-content-between align-items-center bg-white">
+                  <div class="d-flex align-items-center mx-auto">
+                    <div class="login-form">
+                      <form action="NUMERODEPAGE.php" id="login-form" method="post">
+                        <div class="form-group">
+                          <label>Utilisateur</label>
+                          <input type="text" class="form-control" name="utilisateur" placeholder="Utilisateur">
+                        </div>
+                        <div class="form-group">
+                          <label>Mot de passe</label>
+                          <input type="password" class="form-control" name="motdepasse" placeholder="Mot de passe">
+                        </div>
+                        <div>
+                          <button type="submit" name="page" value=NUMERODEPAGE class="btn btn-success data-bs-toggle="modal" data-bs-target="#myModal"">Se connecter</button>
+                        </div>
+                      </form>
+                      <div class="d-flex justify-content-between w-100 mt-3">
+                        <div> <a href="creerprofil5.php">Pas de profil ?</a> </div>
+                        <div> <a href="creerprofil.php">Mot de passe oublié ?</a> </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-
+              
               <!-- Modal footer -->
               <div class="modal-footer bg-light">
                 <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fermer</button>
