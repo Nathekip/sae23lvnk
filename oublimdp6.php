5dd64db754a9723e1a7f241e7e118c160790b035
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-24 10:27:04 +0200

    Update oublimdp6.php

--- oublimdp6.php
+++ oublimdp6.php
@@ -11,30 +11,33 @@
     <div class="container d-flex flex-column">
       <div class="row align-items-center justify-content-center
           min-vh-100 g-0">
-        <div class="col-12 col-md-8 col-lg-4 border-top border-3 border-danger-50">
+        <div class="col-12 col-md-8 col-lg-4 border-top border-3 border-primary">
           <div class="card shadow-sm">
             <div class="card-body">
               <div class="mb-4">
-                <h5>Forgot Password?</h5>
-                <p class="mb-2">Enter your registered email ID to reset the password
+                <h5>Mot de passe oublié ?</h5>
+                <p class="mb-2">Entrez votre adresse mail afin de recevoir un mail de récupération
                 </p>
               </div>
-              <form>
+              <form action="oublimdp6.php" method="post">
                 <div class="mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <input type="email" id="email" class="form-control" name="email" placeholder="Enter Your Email" required="">
+                  <label for="email" class="form-label">Votre adresse mail</label>
+                  <input type="email" id="email" class="form-control" name="email" placeholder="Entrez votre email" required="">
                 </div>
                 <div class="mb-3 d-grid">
                   <button type="submit" class="btn btn-primary">
                     Reset Password
                   </button>
                 </div>
-                <span>Don't have an account? <a href="sign-in.html">sign in</a></span>
+                <span>Pas de profil ? <a href="creerprofil5.php">S'inscrire</a></span>
               </form>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <?php
+      pr();
+    ?>
   </body>
 </html>
