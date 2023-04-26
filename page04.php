dad3e0f4182dc9ca442cfa58f36796838e779b78
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-26 14:33:22 +0200

    Update page04.php

--- page04.php
+++ page04.php
@@ -6,6 +6,15 @@
     setup();
     pageheader();
     pagenavbar("p04");
+    if ( $_POST['oop'] == 'test' ){
+      echo "aaaaaaaaaaaaaaaaaaaa";
+      $_SESSION['PhaseMdp'] == False;                  
+    }
     ?>
+    <form method="post" action="oublimdp6.php">
+      <button type="submit" value="test" name"oop" class="btn btn-link">
+        Revenir en arrière
+      </button>
+    </form>
   </body>
 </html>
