caac94ba78606af6000a30587daf838446e390b5
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-26 11:40:43 +0200

    Update oublimdp6.php

--- oublimdp6.php
+++ oublimdp6.php
@@ -104,6 +104,7 @@
                     <p class="text-black-50 pt-2">Vous pouvez vous connecter à présent
                         </p>
                       </div>
+                      <div class="mb-3 d-grid">
                         <button type="submit" class="btn text-white btn-outline-warning btn-dark">
                             Se connecter
                           </button>                        
