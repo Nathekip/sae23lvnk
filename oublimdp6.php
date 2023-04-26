6e5a1c53790630bfe9db86fd2589a990dfafa787
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-26 15:21:17 +0200

    Update oublimdp6.php

--- oublimdp6.php
+++ oublimdp6.php
@@ -22,6 +22,7 @@
       
                 if ( isset($_POST['RtrMail']) ){
                   $_SESSION['PhaseMdp'] = False;
+                  $_SESSION['usermodif'] = "";
                 }
       
                 # phase email - Test
