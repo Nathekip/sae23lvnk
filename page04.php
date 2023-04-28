9079a4ec070a27f64f4b9232b6034a83343c90e9
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-28 11:25:50 +0200

    Update page04.php

--- page04.php
+++ page04.php
@@ -18,14 +18,14 @@
                   <form action="page04.php" method="post">
                     Votre nouveau mot de passe
                     <div class="pt-1 mb-3 input-group">
-                      <input type="" id="mdp" value="PhrMdp" class="form-control" name="mdp" placeholder="Entrez votre nouveau mdp">
+                      <input type=PhrMdpoeil id="mdp" value="PhrMdp" class="form-control" name="mdp" placeholder="Entrez votre nouveau mdp">
                       <button type="submit" class="btn btn-warning" name="mdpoeil" value=True>
                         <i class="fa-solid fa-eye"></i>
                       </button>                      
                     </div>
                     Confirmez votre nouveau mot de passe
                     <div class="pt-1 mb-3 input-group">
-                      <input type="PhrMdpoeil" id="cmdp" value="PhrCmdp" class="form-control" name="cmdp" placeholder="Confirmez votre mdp">
+                      <input type="PhrCmdpoeil" id="cmdp" value="PhrCmdp" class="form-control" name="cmdp" placeholder="Confirmez votre mdp">
                       <button type="submit" class="btn btn-warning" name="cmdpoeil" value=True>
                         <i class="fa-solid fa-eye"></i>
                       </button>
