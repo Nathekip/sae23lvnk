32b3f68ff3b485a20bf1f48cd91011a9f41eff60
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-28 10:40:20 +0200

    Update page04.php

--- page04.php
+++ page04.php
@@ -13,27 +13,35 @@
           <div class="card shadow-sm border-warning">
             <div class="card-body">
               <div class="mb-4">
-                <form action="oublimdp6.php" method="post">
-                  Votre nouveau mot de passe
-                  <div class="pt-1 mb-3 input-group">
-                    <input type="password" id="mdp" value="PhrMdp" class="form-control" name="mdp" placeholder="Entrez votre nouveau mdp">
-                    <button type="submit" class="btn btn-warning" name="mdpoeil" value=True>
-                      <i class="fa-solid fa-eye"></i>
-                    </button>                      
-                  </div>
-                  Confirmez votre nouveau mot de passe
-                  <div class="pt-1 mb-3 input-group">
-                    <input type="password" id="cmdp" class="form-control" name="cmdp" placeholder="Confirmez votre mdp">
-                    <button type="submit" class="btn btn-warning" name="cmdpoeil" value=True>
-                      <i class="fa-solid fa-eye"></i>
-                    </button>
-                  </div>
-                  <div class="pt-2 mb-3 d-grid">
-                      Réinitialiser le mot de passe
-                    </button>
-                  </div>
+                <form method="post" action="oublimdp6.php">
+                  <button type="submit" name="RtrMail" class="btn btn-link">
+                    <-- Revenir en arrière
+                  </button>
                 </form>
+                <h5>Mot de passe oublié ?</h5>
+                <p class="text-black-50 pt-2">Entrez un nouveau mot de passe afin de changer votre ancien mot de passe
+                </p>
               </div>
+              <form action="oublimdp6.php" method="post">
+                Votre nouveau mot de passe
+                <div class="pt-1 mb-3 input-group">
+                  <input type="password" id="mdp" value="PhrMdp" class="form-control" name="mdp" placeholder="Entrez votre nouveau mdp">
+                  <button type="submit" class="btn btn-warning" name="mdpoeil" value=True>
+                    <i class="fa-solid fa-eye"></i>
+                  </button>                      
+                </div>
+                Confirmez votre nouveau mot de passe
+                <div class="pt-1 mb-3 input-group">
+                  <input type="password" id="cmdp" class="form-control" name="cmdp" placeholder="Confirmez votre mdp">
+                  <button type="submit" class="btn btn-warning" name="cmdpoeil" value=True>
+                    <i class="fa-solid fa-eye"></i>
+                  </button>
+                </div>
+                <div class="pt-2 mb-3 d-grid">
+                    Réinitialiser le mot de passe
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
