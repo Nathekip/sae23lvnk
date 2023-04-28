1380434e692a08d9bb3220d0a1d35112bfd3b7d8
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-28 11:35:21 +0200

    Update oublimdp6.php

--- oublimdp6.php
+++ oublimdp6.php
@@ -66,14 +66,14 @@
                     <form action="oublimdp6.php" method="post">
                     Votre nouveau mot de passe
                     <div class="pt-1 mb-3 input-group">
-                      <input type="password" id="mdp" value="PhrMdp" class="form-control" name="mdp" placeholder="Entrez votre nouveau mdp">
+                      <input type="PhrOeilMdp" id="mdp" value="PhrMdp" class="form-control" name="mdp" placeholder="Entrez votre nouveau mdp">
                       <button type="submit" class="btn btn-warning" name="mdpoeil" value=True>
                         <i class="fa-solid fa-eye"></i>
                       </button>                      
                     </div>
                     Confirmez votre nouveau mot de passe
                     <div class="pt-1 mb-3 input-group">
-                      <input type="password" id="cmdp" class="form-control" name="cmdp" placeholder="Confirmez votre mdp">
+                      <input type="PhrOeilCmdp" id="cmdp" class="form-control" name="cmdp" placeholder="Confirmez votre mdp">
                       <button type="submit" class="btn btn-warning" name="cmdpoeil" value=True>
                         <img src="images/ShPwd.png" alt="Show Password">
                       </button>
@@ -104,20 +104,12 @@
                       </form>';
                         }      
                 # test oeil
-                if ( isset($_POST['mdpoeil']) ){
-                  echo "mdp";
-                  $_SESSION['MdpBool'] = ! $_SESSION['MdpBool'];
-                  echo $_SESSION['MdpBool'];
-                }
-                if ( isset($_POST['cmdpoeil']) ){
-                  echo "cmdp";
-                  $_SESSION['CmdpBool'] = ! $_SESSION['CmdpBool'];
-                  echo $_SESSION['CmdpBool'];
-                }
-                /*if ($_SESSION['MdpBool']){ $formulaire = str_replace("mdpoeil","input",$formulaire);}
-                else { $formulaire = str_replace("mdpoeil","password",$formulaire);}
-                if ($_SESSION['CmdpBool']){ $formulaire = str_replace("cmdpoeil","input",$formulaire);}
-                else { $formulaire = str_replace("mdpoeil","password",$formulaire);}*/
+                if ( isset($_POST['mdpoeil']) ){ $_SESSION['MdpBool'] = ! $_SESSION['MdpBool']; }
+                if ( isset($_POST['cmdpoeil']) ){ $_SESSION['CmdpBool'] = ! $_SESSION['CmdpBool']; }
+                if ($_SESSION['MdpBool']){ $formulaire = str_replace("PhrOeilMdp","input",$formulaire);}
+                else { $formulaire = str_replace("PhrOeilMdp","password",$formulaire);}
+                if ($_SESSION['CmdpBool']){ $formulaire = str_replace("PhrOeilCmdp","input",$formulaire);}
+                else { $formulaire = str_replace("PhrOeilCmdp","password",$formulaire);}
       
                 # phase mdp - Test
                 if (  ( ( strlen( $_POST['mdp'] ) < 8 ) or ( ! preg_match('/[\'^£$%&?*()}{@#~><>,|=_+¬-]/', $_POST['mdp']) ) or ( ! preg_match('/[A-Z]/', $_POST['mdp']) ) ) && isset($_POST['envoi']) )      {                
