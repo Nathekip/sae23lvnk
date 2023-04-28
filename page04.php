147b5ccf0a0dc4c143908b03f6a431f059e278a7
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-04-28 10:37:00 +0200

    Update page04.php

--- page04.php
+++ page04.php
@@ -8,26 +8,38 @@
     pagenavbar("p04");
     pr();
     ?>
-    <form action="oublimdp6.php" method="post">
-      Votre nouveau mot de passe
-      <div class="pt-1 mb-3 input-group">
-        <input type="password" id="mdp" value="PhrMdp" class="form-control" name="mdp" placeholder="Entrez votre nouveau mdp">
-        <button type="submit" class="btn btn-warning" name="mdpoeil" value=True>
-          <i class="fa-solid fa-eye"></i>
-        </button>                      
+    <div class="container d-flex flex-column">
+      <div class="row align-items-center justify-content-center min-vh-100 g-0">
+        <div class="col-12 col-md-8 col-lg-4">
+          <div class="card shadow-sm border-warning">
+            <div class="card-body">
+              <div class="mb-4">
+                <form action="oublimdp6.php" method="post">
+                  Votre nouveau mot de passe
+                  <div class="pt-1 mb-3 input-group">
+                    <input type="password" id="mdp" value="PhrMdp" class="form-control" name="mdp" placeholder="Entrez votre nouveau mdp">
+                    <button type="submit" class="btn btn-warning" name="mdpoeil" value=True>
+                      <i class="fa-solid fa-eye"></i>
+                    </button>                      
+                  </div>
+                  Confirmez votre nouveau mot de passe
+                  <div class="pt-1 mb-3 input-group">
+                    <input type="password" id="cmdp" class="form-control" name="cmdp" placeholder="Confirmez votre mdp">
+                    <button type="submit" class="btn btn-warning" name="cmdpoeil" value=True>
+                      <img src="images/ShPwd.png" alt="Show Password">
+                    </button>
+                  </div>
+                  <div class="pt-2 mb-3 d-grid">
+                    <i class="fa-solid fa-eye"></i>
+                      Réinitialiser le mot de passe
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      Confirmez votre nouveau mot de passe
-      <div class="pt-1 mb-3 input-group">
-        <input type="password" id="cmdp" class="form-control" name="cmdp" placeholder="Confirmez votre mdp">
-        <button type="submit" class="btn btn-warning" name="cmdpoeil" value=True>
-          <img src="images/ShPwd.png" alt="Show Password">
-        </button>
-      </div>
-      <div class="pt-2 mb-3 d-grid">
-        <i class="fa-solid fa-eye"></i>
-          Réinitialiser le mot de passe
-        </button>
-      </div>
-    </form>
+    </div>
   </body>
 </html>
