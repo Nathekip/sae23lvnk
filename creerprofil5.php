59da0fbae64151ef8f49212ef561c4605ae701ec
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-04 11:52:08 +0200

    Update creerprofil5.php

--- creerprofil5.php
+++ creerprofil5.php
@@ -56,11 +56,13 @@
                                             <div class="d-flex flex-row align-items-center mb-4">
                                               <i class="fas fa-person-circle-question fa-lg me-3 fa-fw"></i>
                                               <div class="form-outline flex-fill mb-0">
-                                                <label class="form-label" for="cmdp">
-                                                   <select class="form-select-sm" id="sel1" name="sellist1">
-                                                      <option>Quel est le nom de votre premier animal ?</option>
-                                                      <option>Dans quelle ville êtes vous né ?</option>
-                                                      <option>Quel est le premier concert que vous avez vu ?</option>
+                                                <label class="form-label" for="rep">
+                                                   <select class="form-select-sm" id="question" name="question">
+                                                      <datalist id="question">
+                                                          <option>Quel est le nom de votre premier animal ?</option>
+                                                          <option>Dans quelle ville êtes vous né ?</option>
+                                                          <option>Quel est le premier concert que vous avez vu ?</option>
+                                                      </datalist>
                                                     </select>                                                                                                              
                                                 </label>
                                                 <input value="phrRep" type="input" placeholder="Réponse à la question" name="rep" id="rep" class="form-control" />
