a7299c271e6a31ff56d660f102d6e8e6348ac766
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-04 11:11:57 +0200

    Update creerprofil5.php

--- creerprofil5.php
+++ creerprofil5.php
@@ -53,6 +53,20 @@
                                                 <input value="phrCmdp" type="password" placeholder="Répétez votre mot de passe" name="cmdp" id="cmdp" class="form-control" />
                                               </div>
                                             </div>
+                                            <div class="d-flex flex-row align-items-center mb-4">
+                                              <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                                              <div class="form-outline flex-fill mb-0">
+                                                <label class="form-label" for="cmdp">Répéter votre Mot de Passe :</label>
+                                                <input value="phrCmdp" type="password" placeholder="Répétez votre mot de passe" name="cmdp" id="cmdp" class="form-control" />
+                                              </div>
+                                            </div>
+                                            <div class="d-flex flex-row align-items-center mb-4">
+                                              <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                                              <div class="form-outline flex-fill mb-0">
+                                                <label class="form-label" for="cmdp">Répéter votre Mot de Passe :</label>
+                                                <input value="phrCmdp" type="password" placeholder="Répétez votre mot de passe" name="cmdp" id="cmdp" class="form-control" />
+                                              </div>
+                                            </div>
                                             <div class="form-check d-flex justify-content-center mb-5">
                                               <input class="form-check-input me-2" name="condu" type="checkbox" value="" id="condu" />
                                               <label class="form-check-label" for="condu">
