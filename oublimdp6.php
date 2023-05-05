b12147fa639317fba8c4db4f7fc36e2d4633af27
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-05 11:18:32 +0200

    Update oublimdp6.php

--- oublimdp6.php
+++ oublimdp6.php
@@ -153,8 +153,10 @@
                 # question de sécurité
                 $question = ["Quel était le nom de votre premier animal de compagnie ?","Dans quelle ville êtes vous né ?","Quel est le premier artiste ou groupe que vous avez vu en concert ?"];
                 #$clequestion = int($_SESSION['usermodif']['question'])-1;
-                $clequestion=3-1;
-                $formulaire = str_replace("PlaceholderQuestionSecurite",$question[$clequestion], $formulaire);
+                echo $_SESSION['usermodif']['question'];
+                echo int($_SESSION['usermodif']['question']);
+                echo int($_SESSION['usermodif']['question'])-1;
+                #$formulaire = str_replace("PlaceholderQuestionSecurite",$question[$clequestion], $formulaire);
       
                 # test oeil
                 if ( isset($_POST['mdpoeil']) ){ $_SESSION['MdpBool'] = ! $_SESSION['MdpBool']; }
