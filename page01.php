1b6da968a719f90dfbeba0b55752e046ffd68431
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-10 13:40:51 +0200

    Update page01.php

--- page01.php
+++ page01.php
@@ -7,7 +7,7 @@
       pagenavbar("p01");
       #pr();
     ?>
-    <div class="pt-5 position-relative">
+    <div class="position-relative">
       <div class="position-absolute bg-gradient-primary text-white border-0 rounded-0 p-3 w-100 d-flex align-items-center justify-content-center" style="top: 25%">
         <div class="position-relative text-center">
           <h1 class="text-white text-uppercase fw-bold mb-4 text-shadow" style="text-shadow: 0 0 10px black;">Bienvenue chez CarFusion</h1>
