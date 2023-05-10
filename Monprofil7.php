2513a18b9082519f1acd8315b06cdbc90a3fb063
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-10 08:44:59 +0200

    Update Monprofil7.php

--- Monprofil7.php
+++ Monprofil7.php
@@ -38,7 +38,7 @@
       # Procédure d'enregistrement de l'image 
       $target_dir = "pp/";
       #$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-      $target_file = $target_dir . "Test" . strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+      $target_file = $target_dir . "Test" . ".jpeg";
       echo $target_file;
       echo "<br>";
       echo basename($_FILES["fileToUpload"]["name"]);
