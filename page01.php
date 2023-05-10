c3151a65620410eb57f2f43910fa2d48439f79b3
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-10 13:21:51 +0200

    Update page01.php

--- page01.php
+++ page01.php
@@ -5,7 +5,7 @@
       include('fonctions.php');
       setup();
       pagenavbar("p01");
-      pr();
+      #pr();
     ?>
     <div class="pt-5 position-relative">
       <div class="position-absolute bg-gradient-primary text-white border-0 rounded-0 p-3 w-100 d-flex align-items-center justify-content-center" style="top: 25%">
@@ -16,6 +16,11 @@
       </div>
       <img src="images/banniere.png" alt="Bannière" class="w-100">
     </div>
+    <div class="container">
+      <div class="pt-2 border-top border-dark border-3 my-1"></div>
+    </div>
+    <h2 class="fw-bold pt-3 pb-1 text-uppercase text-center">Evolution de l'entreprise *</h2>
+    <br>
 
   </body>
 </html>
