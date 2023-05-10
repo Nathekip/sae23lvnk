15b72c87b8126af8873901266649c010f05ba0a8
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-10 13:16:09 +0200

    Update page01.php

--- page01.php
+++ page01.php
@@ -7,5 +7,15 @@
       pagenavbar("p01");
       pr();
     ?>
+    <div class="pt-5 position-relative">
+      <div class="position-absolute bg-gradient-primary text-white border-0 rounded-0 p-3 w-100 d-flex align-items-center justify-content-center" style="top: 25%">
+        <div class="position-relative text-center">
+          <h1 class="text-white text-uppercase fw-bold mb-4 text-shadow" style="text-shadow: 0 0 10px black;">Bienvenue chez CarFusion</h1>
+          <h2 class="text-white text-uppercase fw-bold text-shadow" style="text-shadow: 0 0 10px black;">Heureux de vous voir ici !</h2>
+        </div> 
+      </div>
+      <img src="assets/images/banniere.png" alt="Bannière" class="w-100">
+    </div>
+
   </body>
 </html>
