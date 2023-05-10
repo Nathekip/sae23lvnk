156d7485aaf60b3336e5c262ec9847610aca82d2
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-10 09:08:27 +0200

    Update Monprofil7.php

--- Monprofil7.php
+++ Monprofil7.php
@@ -6,11 +6,11 @@
       setup();
       pagenavbar("p07");
     ?>
-    <form action="Monprofil7.php" method="post" enctype="multipart/form-data">
+    <!-- <form action="Monprofil7.php" method="post" enctype="multipart/form-data">
       Select image to upload:
       <input type="file" name="fileToUpload" id="fileToUpload">
       <input type="submit" value="Upload Image" name="submit">
-    </form>
+    </form> -->
     
     
     <?php
