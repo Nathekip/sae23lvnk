7aad31d3e671ce3cf1b0460b7831a340038a7a2c
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-10 09:43:47 +0200

    Update Monprofil7.php

--- Monprofil7.php
+++ Monprofil7.php
@@ -109,7 +109,7 @@
       // if everything is ok, try to upload file
       } else {
         if (move_uploaded_file($_FILES["Upload"]["tmp_name"], $target_file)) {
-          echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+          echo "The file ". htmlspecialchars( basename( $_FILES["Upload"]["name"])). " has been uploaded.";
         } else {
           echo "Sorry, there was an error uploading your file.";
         }
