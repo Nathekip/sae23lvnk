2a5a106854318909230590c36fd03abef5ea5152
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-10 13:22:14 +0200

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -77,7 +77,7 @@ function pagenavbar($page=""){
   }
 	
   #fixed-top
-  $navbar = '<nav class="navbar navbar-expand-lg bg-black navbar-dark">
+  $navbar = '<nav class="navbar navbar-expand-lg fixed-top bg-black navbar-dark">
 	       <div class="container">
 	         <a class="navbar-brand" href="page01.php"><img src="images/Logo.png" alt="Logo CarFusion"></a>
 	         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
