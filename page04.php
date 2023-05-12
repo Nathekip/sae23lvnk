981ae4859ca374136aadce6595bc827f3eaa1f66
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-12 11:59:32 +0200

    Update page04.php

--- page04.php
+++ page04.php
@@ -5,18 +5,6 @@
     include('fonctions.php');
     setup();
     pagenavbar("p04");
-    $a = "974 ------- La trix";
-    echo "$a|";
-    echo "<br>";
-    $c = trim(substr($a, 0, 3)," ");
-    echo "$c|";
-    echo "<br>";
-    
-    $a = "44 ------- La trix";
-    echo "$a|";
-    echo "<br>";
-    $c = trim(substr($a, 0, 3)," ");
-    echo "$c|";
     pr();
     ?>
   </body>
