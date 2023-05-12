aedce28f6f9d567cc8a08e2189d1e2daaec66194
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-12 11:54:21 +0200

    Update page04.php

--- page04.php
+++ page04.php
@@ -5,6 +5,14 @@
     include('fonctions.php');
     setup();
     pagenavbar("p04");
+    echo "<br>";
+    echo "44 ";
+    echo "<br>";
+    $a = substr("44 ", 0, 3);
+    echo $a;
+    echo "<br>";
+    $b = trim($a," ");
+    echo $b;
     pr();
     ?>
   </body>
