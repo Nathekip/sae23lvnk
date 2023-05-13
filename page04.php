75f8fba91cb0cec51f8860f3890a93ff8fe6fab1
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-05-13 10:44:43 +0100

    Update page04.php

--- page04.php
+++ page04.php
@@ -5,7 +5,12 @@
     include('fonctions.php');
     setup();
     pagenavbar("p04");
-    pr();
+    #addUser($_POST['utilisateur'], $_POST['mdp'], $_POST['mail'], $_POST['dep'],"user", $_POST['question'], $_POST['reponse']);
+    addUser('utilisateur','mdp', 'mail', 'dep',"user", 'question', 'reponse');
+    echo "<pre>";
+    $json = file_get_contents('data/users.json');
+    $user = json_decode($json, true);
+    print_r($user);
     ?>
   </body>
 </html>
