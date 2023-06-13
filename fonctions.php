399bc4e2e6b59029fdfbd0c71506691f82ab9e7c
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-06-13 09:53:35 +0100

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -211,7 +211,7 @@ function pagenavbar($page=""){
                           $(modal).modal("handleUpdate"); // Actualiser le modal après la soumission du formulaire          
                           if (xhr.responseText.indexOf("Erreur") === -1) {
                             window.location.assign(';
-                            echo '"http://SAE23LVNK/'.basename($_SERVER['PHP_SELF']).'"';
+                            echo '"http://FeUrEuR/'.basename($_SERVER['PHP_SELF']).'"';
                           echo ');            
                           }
                         }
