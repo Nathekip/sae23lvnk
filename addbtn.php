9604899cdc4b95a8a0dd99b8ca1c5b5011465703
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-06-14 10:33:32 +0100

    Delete addbtn.php

--- addbtn.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include('Vue/fonctions.php');
-if ($_POST['mdp']==$_POST['cmdp']){
-    addUser($_POST['pseudo'],$_POST['mdp'],$_POST['role']);
-}
-
-header('Location: page06.php');
-?>
