fef5022781329a3769e7dfd8ea5b33f128328ab2
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-06-14 10:21:37 +0100

    Update users.php

--- Modèle/users.php
+++ Modèle/users.php
@@ -1,7 +1,7 @@
 <?php
 function addUser($usr, $mdp, $mail, $departement, $role="user",$question=0,$reponse=NULL){
   $user = array();
-  $json = file_get_contents('data/users.json');
+  $json = file_get_contents('../data/users.json');
   $user = json_decode($json, true);
 
   $add = array();
@@ -16,14 +16,14 @@ function addUser($usr, $mdp, $mail, $departement, $role="user",$question=0,$repo
   $user[$add['user']]=$add;
 
   $jsonString = json_encode($user, JSON_PRETTY_PRINT);
-  $fp = fopen("data/users.json", 'w');
+  $fp = fopen("../data/users.json", 'w');
   fwrite($fp, $jsonString);
   fclose($fp);
 }
 
 function readUser(){
   $user = array();
-  $json = file_get_contents('data/users.json');
+  $json = file_get_contents('../data/users.json');
   $user = json_decode($json, true);
   return $user;
 }
