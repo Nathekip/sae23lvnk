8de8fdad274d4659b0246c09717ad60cfb37b7c1
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-06-16 12:08:42 +0200

    Update deconnexion.php

--- Vue/deconnexion.php
+++ Vue/deconnexion.php
@@ -2,6 +2,6 @@
 include('../Vue/fonctions.php');
 setup();
 session_unset();
-$page = "Location: /Controleur/".$_POST['page'].".php";
+$page = "Location: /sae23lvnk/Controleur/".$_POST['page'].".php";
 header($page);
 ?>
