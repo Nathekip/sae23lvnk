efcfe2c167121f1a0056563b6c99fc7a777126e0
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-06-16 10:30:08 +0100

    Update partenaire.php

--- Modele/partenaire.php
+++ Modele/partenaire.php
@@ -1,23 +1,38 @@
 <?php
 // Fonction pour supprimer un partenaire du fichier JSON
-    function deletePartner($partnerName) {
-        $json_data = file_get_contents('../Vue/partenaire.json');
-        $partners = json_decode($json_data, true);
-        
-        foreach ($partners as $key => $partner) {
-            if ($partner['nom'] === $partnerName) {
-                // Supprimer la photo du partenaire du dossier images/partenaire
-                $photoPath = '../images/partenaire/' . basename($partner['photo']);
-                if (file_exists($photoPath)) {
-                    unlink($photoPath);
-                }
-
-                unset($partners[$key]);
-                break;
+function deletePartner($partnerName) {
+    $partners = getPartenaire();
+    
+    foreach ($partners as $key => $partner) {
+        if ($partner['nom'] === $partnerName) {
+            // Supprimer la photo du partenaire du dossier images/partenaire
+            $photoPath = '../images/partenaire/' . basename($partner['photo']);
+            if (file_exists($photoPath)) {
+                unlink($photoPath);
             }
-        }
 
-        $updated_json_data = json_encode($partners, JSON_PRETTY_PRINT);
-        file_put_contents('../Vue/partenaire.json', $updated_json_data);
+            unset($partners[$key]);
+            break;
+        }
     }
+}
+function addPartenaire($nom,$description,$photo){
+    $partners = getPartenaire();
+
+    $new_partner = [
+        'nom' => $nom,
+        'description' => $description,
+        'photo' => $photo_destination
+    ];
+
+    $partners[] = $new_partner;
+
+    $updated_json_data = json_encode($partners, JSON_PRETTY_PRINT);
+    file_put_contents('../Vue/partenaire.json', $updated_json_data);
+}
+function getPartenaire(){
+    $json_data = file_get_contents('../Vue/partenaire.json');
+    $partners = json_decode($json_data, true);
+    return $partners;
+}
 ?>
