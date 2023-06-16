c4f3e5c9e50072f4476111018607857820e29de9
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-06-16 08:08:01 +0100

    Update crea_users.php

--- Controleur/crea_users.php
+++ Controleur/crea_users.php
@@ -1,9 +1,10 @@
 <?php
 //include('../Modele/users.php');
-//include('../Vue/fonctions.php');
+include('../Vue/fonctions.php');
 
 //newUsers();
 echo "<pre> test";
+pr();
 //$user = readUsers();
 //print_r($user);
 ?>
