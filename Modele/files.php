2e7b7fe60d83fd823318c0ebe2518c7d005c36b5
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-06-17 15:55:09 +0100

    Update files.php

--- Modele/files.php
+++ Modele/files.php
@@ -4,3 +4,38 @@ function getFiles(){
   $files = json_decode($json, true);
   return $files;
 }
+
+function writeFiles($files){
+    $jsonString = json_encode($files, JSON_PRETTY_PRINT);
+    $fp = fopen("../data/files.json", 'w');
+    fwrite($fp, $jsonString);
+    fclose($fp);
+}
+
+function delfiltest($filepath){
+    unlink($filepath);
+    $files = getFiles();
+  
+    for($i=0; $i < count($files); $i++){
+      if ($files[$i]['path'] == $filepath){
+        unset($files[$i]);
+      }
+    } 
+    writeFiles($files);   
+  }
+  
+  function deleteFile($deletefile){
+    echo "test";
+    foreach($deletefile as $file){
+      unlink($file);
+      $files = getFiles();
+  
+      foreach ($files as $key => $value){
+        if ($files[$key]['path'] == $file){
+          unset($files[$key]);
+        }
+      }
+    }
+    writeFiles($files);
+  }
+?>
