b8018c12ba5cf15e0a3dd6b1e4dee72153923fd3
louis-crtl <130141493+louis-crtl@users.noreply.github.com>
2023-06-17 12:45:32 +0200

    Update deconnexion.php

--- Vue/deconnexion.php
+++ Vue/deconnexion.php
@@ -2,6 +2,6 @@
 include('../Vue/fonctions.php');
 setup();
 session_unset();
-$page = "Location: /sae23lvnk/Controleur/".$_POST['page'].".php";
+$page = "Location: /sae23lvnk-main/Controleur/".$_POST['page'].".php";
 header($page);
 ?>
