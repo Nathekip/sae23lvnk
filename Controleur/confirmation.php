438d2ccda2b120b5350335d50691adb8efb7aff7
Nathekip <130141751+Nathekip@users.noreply.github.com>
2023-06-18 19:33:26 +0100

    Update confirmation.php

--- Controleur/confirmation.php
+++ Controleur/confirmation.php
@@ -31,7 +31,7 @@
         </ul>
         <br>
         <p>Votre réservation a été effectuée avec succès. Nous vous remercions pour votre confiance et vous souhaitons une agréable expérience avec notre voiture.</p>
-        <p>Votre voiture sera disponible dans notre agence de <?php echo $_POST['ville']; ?>, n'hésitez pas à suivre son avancement.</p>
+        <p>Votre voiture sera disponible dans notre agence de <strong><?php echo $_POST['ville']; ?></strong>, n'hésitez pas à suivre son avancement.</p>
       </div>
     </div>
   </div>
